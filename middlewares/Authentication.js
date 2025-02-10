36699064cce5061c88a34ccab57c3f36455dc133
UNIGO777 <naman13399@gmail.com>
2025-02-11 03:25:18 +0530

    aisjdhf

--- middlewares/Authentication.js
+++ middlewares/Authentication.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import User from '../models/user.js';
+import User from '../Models/User.js';
 import Seller from '../Models/Seller.js';
 
 
